b1f99ab06d39a9d16258c4be17c3a0fba6fcc4e4
Adam Moran <jeffadammoran@gmail.com>
2019-02-08 22:03:12 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect (array)
+  new_array = []
+  new_string =""
+  i=0
+  while i < array.length
+    new_string = yield array[i]
+    new_array << new_string
+    i+=1
+  end
+  new_array
+end
